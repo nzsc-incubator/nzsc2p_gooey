984c460cd232827cf13c97fa402d659e067012a9
Kyle Lin <kylejlin.us@gmail.com>
2018-06-14 23:12:15 -0700

    Refactor

--- web-src/index.js
+++ web-src/index.js
@@ -1,5 +1,9 @@
+import { NZSCTwoPlayerGameWebInterface } from './wasm/nzsc2p_gooey';
+import clownkit from './clownkit';
+import generateRoomName from './generateRoomName';
 import { correctCanvasDimensions } from './canvas';
-import new2PGame from './new2PGame';
+import * as animations from './animations';
+import nothingToCharacter from './transitions/nothingToCharacter';
 
 window.addEventListener('resize', () => {
   correctCanvasDimensions();
@@ -8,4 +12,35 @@ window.addEventListener('resize', () => {
   }*/
 });
 
-new2PGame();
+(async () => {
+  const store = {
+    game: NZSCTwoPlayerGameWebInterface.new(),
+    roomName: null,
+    aOrB: null,
+  };
+
+  animations.loading.start();
+
+  const roomNames = await clownkit.list();
+
+  if (roomNames.length > 0) {
+    const [roomName] = roomNames;
+
+    store.roomName = roomName;
+    store.aOrB = 'B';
+
+    await clownkit.join(roomName);
+
+    nothingToCharacter();
+  } else {
+    const roomName = generateRoomName();
+
+    store.roomName = roomName;
+    store.aOrB = 'A';
+
+    await clownkit.create(roomName);
+    await clownkit.waitForRoomToBeFull(roomName);
+
+    nothingToCharacter();
+  }
+})();
