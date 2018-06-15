e43f282ff9f13641dd707a1e43d54f5c1e431cd2
Kyle Lin <kylejlin.us@gmail.com>
2018-06-14 20:11:03 -0700

    Copy setup from first attempt

---
+++ web-src/index.js
@@ -0,0 +1,3 @@
+import { NZSCTwoPlayerGameWebInterface } from './wasm/nzsc2p_gooey';
+
+alert('Hello nzsc2p!');
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+//const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+module.exports = {
+  entry: './web-src/load-index-async.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  /*
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      staticFileGlobs: ['public/index.html', 'public/index.css'],
+      mergeStaticsConfig: true,
+      stripPrefix: 'public/'
+    })
+  ]
+  */
+};
