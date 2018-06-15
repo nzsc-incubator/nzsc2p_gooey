311816eeafdb6fc471b4ca8e0244b4fb466d6288
Kyle Lin <kylejlin.us@gmail.com>
2018-06-14 21:30:43 -0700

    Correct canvas dimensions onresize

--- web-src/index.js
+++ web-src/index.js
@@ -1,4 +1,12 @@
 import { NZSCTwoPlayerGameWebInterface } from './wasm/nzsc2p_gooey';
+import { correctCanvasDimensions } from './canvas';
 import * as animations from './animations/index';
+
+window.addEventListener('resize', () => {
+  correctCanvasDimensions();
+  /*if (store.repaint) {
+    store.repaint();
+  }*/
+});
 
 animations.loading.start();
